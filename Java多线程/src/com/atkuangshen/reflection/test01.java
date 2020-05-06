7a4d79b841c43bd514832f48e2803e90ccef0d9d
mrzhaojunjie <1069144207@qq.com>
2020-05-06 10:05:43 +0800

    初识反射

---
+++ Java多线程/src/com/atkuangshen/reflection/test01.java
@@ -0,0 +1,97 @@
+package com.atkuangshen.reflection;
+
+public class test01 {
+    public static void main(String[] args) throws Exception {
+        Person p1 = new Student();
+        System.out.println("这个人是"+p1.name);
+
+        Class c1 = Student.class;
+        Class c2 = Class.forName("com.atkuangshen.reflection.Student");
+        Class c3 = p1.getClass();
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c3.hashCode());
+    }
+}
+class User{
+    private String name;
+    private int age;
+    private String sex;
+
+    public User() {
+    }
+
+    public User(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
+class Person{
+    public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+class Student extends Person{
+    public Student(){
+        this.name = "学生";
+    }
+}
+class Teacher extends Person{
+    public Teacher(){
+        this.name = "老师";
+    }
+}
