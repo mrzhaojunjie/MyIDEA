a6b2bf799d66914c7aa17d26c1b53e7b3b6c722d
mrzhaojunjie <1069144207@qq.com>
2020-06-06 17:09:29 +0800

    Session

---
+++ Java-Maven-02/src/main/java/com/sky/Servlet/servlet/SessionDemo02.java
@@ -0,0 +1,31 @@
+package com.sky.Servlet.servlet;
+
+import com.sky.Servlet.pojo.Person;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/s2")
+public class SessionDemo02 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("GBK");
+        resp.setCharacterEncoding("GBK");
+        resp.setContentType("text/html");
+
+        HttpSession session = req.getSession();
+
+        Person person = (Person) session.getAttribute("name");
+        System.out.println(person.toString());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
